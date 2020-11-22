06ef2c320e67f25b34705ef815c1557fb0e36235
Ryan DiMascio <ryand@channelbakers.com>
2020-11-21 22:30:30 -0800

    feat: finish header & hero desktop styles

--- header.php
+++ header.php
@@ -4,11 +4,13 @@ wp_head();
 
 ?>
 
-<header>
-    <div class="logo">
-        <?php get_template_part('template-parts/site-logo'); ?>
-    </div>
-    <div class="menu">
-        <?php get_template_part('template-parts/menu-icon'); ?>
+<header class="header">
+    <div class="header-wrapper">
+        <div class="logo">
+            <?php get_template_part('template-parts/site-logo'); ?>
+        </div>
+        <div class="menu">
+            <?php get_template_part('template-parts/menu-icon'); ?>
+        </div>
     </div>
 </header>
--- template-parts/hero.php
+++ template-parts/hero.php
@@ -2,20 +2,24 @@
 
 ?>
 
-<div class="hero-wrapper">
-    <div class="hero-content-wrapper">
+<div class="hero">
+    <div class="hero-wrapper">
         <div class="hero-content">
-            <p class="title-prefix">Who we are</p>
-            <h1>Engage brand and increase viewability.</h1>
-            <p>Maecenas finibus urna a accumsan dapibus. Donec lobortis, neque vel vehicula dictum, dolor magna maximus ante, ac pulvinar sem leo quis sem.</p>
-            <div class="hero-buttons">
-                <a href="/">Download Now</a>
-                <a href="/">Learn More</a>
+            <div class="hero-content-wrapper">
+                <p class="title-prefix">Who we are</p>
+                <h1>Engage brand and increase viewability.</h1>
+                <p>Maecenas finibus urna a accumsan dapibus. Donec lobortis, neque vel vehicula dictum, dolor magna maximus ante, ac pulvinar sem leo quis sem.</p>
+                <div class="hero-buttons">
+                    <a href="/">Download Now</a>
+                    <a href="/">Learn More</a>
+                </div>
             </div>
         </div>
-    </div>
 
-    <div class="hero-image-wrapper">
-        <div class="hero-image"></div>
+        <div class="hero-image">
+            <div class="hero-image-wrapper">
+                <img src="<?= get_template_directory_uri() . '/assets/images/Device_Ipad.png' ?>" alt="">
+            </div>
+        </div>
     </div>
 </div>
